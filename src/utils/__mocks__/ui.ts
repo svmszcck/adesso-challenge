e877c1fc273bcbf5bc4214f7c87bcbfea91471ef
Onur <onurdemirtas91@gmail.com>
2024-10-29 16:41:58 +0300

    update mock

--- src/utils/__mocks__/ui.ts
+++ src/utils/__mocks__/ui.ts
@@ -2,6 +2,8 @@ const mockedUiUtils = {
   showAlert: jest.fn(),
   showErrorAlert: jest.fn(),
   showSuccessAlert: jest.fn(),
+  screenWidth: 400,
+  screenHeight: 800,
 };
 
 export default mockedUiUtils;
