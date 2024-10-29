9d44bc5264d02a3e23afc95e8d67a82df5b18a44
Onur <onurdemirtas91@gmail.com>
2024-10-29 23:56:38 +0300

    update alert message text

--- src/constants/alertMessages.ts
+++ src/constants/alertMessages.ts
@@ -9,7 +9,7 @@ enum AlertMessages {
   FAV_LIST_EMPTY_MESSAGE = "Favori listeniz boş.",
   FAV_ADDED_MESSAGE = "Film favorilere eklendi.",
   FAV_REMOVED_MESSAGE = "Film favorilerden kaldırıldı.",
-  SEARCH_INVALID_MESSAGE = "Lütfen bir arama terimi girin.",
+  SEARCH_INVALID_MESSAGE = "Lütfen bir arama terimi girin veya filtre ekleyin.",
 }
 
 export default AlertMessages;
